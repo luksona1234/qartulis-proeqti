f4d3fab1c79692d22d0320ecbe3bedfe4fc9ce50
Luka <lukakhachvani0311@gmail.com>
2022-12-14 20:46:56 +0400

    modified HTML, CSS and JS files

---
+++ mian.js
@@ -0,0 +1,13 @@
+let akaki = document.querySelector('.realismi-akaki')
+let imgAkaki = document.querySelector('.img-wavwavadze')
+
+
+let clik = document.querySelector('.click')
+let imgAakaki = document.querySelector('.img-akaki')
+
+
+clik.addEventListener('click', () => {
+    if(imgAakaki.style.display = 'none'){
+        imgAakaki.style.display = 'block'
+    } 
+})
